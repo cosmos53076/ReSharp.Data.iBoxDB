47b38ebd98c5dec5c70b4b638d196ea35019ead1
Jerry Lee <cosmos53076@163.com>
2020-04-09 13:57:31 +0800

    Initialize unity project.

---
+++ src/QueryLogicalOperator.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Jerry Lee. All rights reserved. Licensed under the MIT License. See LICENSE in the
+// project root for license information.
+
+namespace ReSharp.Data.iBoxDB
+{
+    /// <summary>
+    /// Enum that defines the database query logical operator.
+    /// </summary>
+    public enum QueryLogicalOperator
+    {
+        /// <summary>
+        /// No query logical operator specified.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// The query logical operator of And.
+        /// </summary>
+        And,
+
+        /// <summary>
+        /// The query logical operator of Or.
+        /// </summary>
+        Or
+    }
+}
